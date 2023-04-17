9a85cd255f38eabb39cc46474e4d1cb30904ef2a
bedirhan <celayirbedirhan@gmail.com>
2023-04-17 12:16:42 +0300

    chore: update healthcheck path

--- src/routes/health-check.router.ts
+++ src/routes/health-check.router.ts
@@ -3,6 +3,6 @@ import { Request, Response, Router } from "express";
 
 export const healthCheckRouter = Router();
 
-healthCheckRouter.get("/health-check", (req: Request, res: Response) => {
+healthCheckRouter.get("/healthcheck", (req: Request, res: Response) => {
 	res.send("OK!");
 });
