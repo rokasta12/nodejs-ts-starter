994836a65ea41fc9201e6c1e0b7d7788c5a380af
bedirhan <celayirbedirhan@gmail.com>
2023-04-17 12:39:47 +0300

    up

--- src/index.ts
+++ src/index.ts
@@ -7,10 +7,11 @@ const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.use(express.json());
+
+app.use("/api/v1", appRoutes);
 app.use("/", (req, res, next) => {
 	res.status(200).send("OK");
 });
-app.use("/api/v1", appRoutes);
 
 app.listen(PORT, () => {
 	console.log(`Server is running on port ${PORT}`);
