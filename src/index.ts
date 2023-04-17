b71d16836f93e602187c3e7d8e5cc879ff37dbf7
bedirhan <celayirbedirhan@gmail.com>
2023-04-17 12:28:11 +0300

    chore: update healtcheck route

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,9 @@ const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.use(express.json());
+app.use("/", (req, res, next) => {
+	res.status(200).send("OK");
+});
 app.use("/api/v1", appRoutes);
 
 app.listen(PORT, () => {
